d0e1bee27f9fa7f1a8761e2dacecdc1c48f1a0ad
Mark Pariente <markpariente@gmail.com>
2014-02-17 21:35:08 -0800

    encfs-java update to remove PBKDF2 passwordLen

--- src/org/mrpdaemon/android/encdroid/NativePBKDF2Provider.java
+++ src/org/mrpdaemon/android/encdroid/NativePBKDF2Provider.java
@@ -14,16 +14,16 @@ public class NativePBKDF2Provider extends EncFSPBKDF2Provider {
 		}
 	}
 
-	native byte[] pbkdf2(String password, int saltLen,
-			byte[] salt, int iterations, int keyLen);
+	native byte[] pbkdf2(String password, int saltLen, byte[] salt,
+			int iterations, int keyLen);
 
 	/* calling here forces class initialization */
 	public static void checkAvailable() {
 	}
 
 	@Override
-	public byte[] doPBKDF2(int passwordLen, String password, int saltLen,
-			byte[] salt, int iterations, int keyLen) {
+	public byte[] doPBKDF2(String password, int saltLen, byte[] salt,
+			int iterations, int keyLen) {
 		Log.d("NativePBKDF2Provider", "Calling into native PBKDF2 function!");
 		return pbkdf2(password, saltLen, salt, iterations, keyLen);
 	}
