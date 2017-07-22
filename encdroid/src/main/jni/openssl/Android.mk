c489f40cff02b9613ae70873accb2dd6f739b9cb
Mark Pariente <markpariente@gmail.com>
2017-07-22 01:18:46 -0700

    Upgrade to OpenSSL 1.1.0f
    Picked up prebuilt OpenSSL 1.1.0f for Android from:
    https://github.com/leenjewel/openssl_for_ios_and_android
    
    Switched to using static libraries instead of shared objects.

--- encdroid/src/main/jni/openssl/Android.mk
+++ encdroid/src/main/jni/openssl/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := openssl
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcrypto.so
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcrypto.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
